59571460f9944acd54c9f1b68b55a791f1cf157f
Taisei-rgb <taisei.contact@gmail.com>
2020-10-07 17:49:34 +0900

    スペルミス変更

--- src/templates/Login.jsx
+++ src/templates/Login.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { useDispatch } from "react-redux";
 import { push } from "connected-react-router";
 import { signInAction } from "../reducks/users/actions";
-import { signIn } from "../reducks/users/oparations"
+import { signIn } from "../reducks/users/operations"
 
 const Login = () => {
   const dispatch = useDispatch()
