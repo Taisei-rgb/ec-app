564cf7d389b0673f95aa711f6d62dee08bbca9cd
Taisei-rgb <taisei.contact@gmail.com>
2020-10-07 18:59:38 +0900

    Firebase用の設定ファイルの作成

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDNtCUkY0WQ7A8WOjVPIzzDcMoqyjYqjd0",
+  authDomain: "ec-app-bac4e.firebaseapp.com",
+  databaseURL: "https://ec-app-bac4e.firebaseio.com",
+  projectId: "ec-app-bac4e",
+  storageBucket: "ec-app-bac4e.appspot.com",
+  messagingSenderId: "627802830661",
+  appId: "1:627802830661:web:113c9f866aa3b76349702c",
+  measurementId: "G-HXW39XDNQF"
+};
