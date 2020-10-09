5ca760e36894700ee096d22ed5b3609f278736e0
Taisei-rgb <taisei.contact@gmail.com>
2020-10-09 16:52:45 +0900

    商品情報を保存

--- src/templates/ProductEdit.jsx
+++ src/templates/ProductEdit.jsx
@@ -1,7 +1,10 @@
 import React, { useCallback, useState } from "react";
+import { useDispatch } from "react-redux";
 import { PrimaryButton, SelectBox, TextInput } from "../components/UIkit";
+import { saveProduct } from "../reducks/products/operations";
 
 const ProductEdit = () => {
+  const dispatch = useDispatch();
 
   const [name, setName] = useState(""),
     [description, setDescription] = useState(""),
@@ -62,7 +65,7 @@ const ProductEdit = () => {
         <div className="center">
           <PrimaryButton
             label={"商品情報を保存"}
-          // onClick={}
+            onClick={() => dispatch(saveProduct(name, description, category, gender, price))}
           />
         </div>
       </div>
